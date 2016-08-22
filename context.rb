8992629a5080c487d88ee10a5e74241461487ef1
Sean Jezewski <seanwjezewski@gmail.com>
2016-08-21 19:43:36 -0700

    Add set function

--- context.rb
+++ context.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require 'json'
 
 # Detects if there is any context in the CWD for:
 #
@@ -15,32 +16,55 @@ def usage
 end
 
 def parse_command
-    puts ARGV
-    if ARGV.size != 1
+    if ARGV.size == 0
         usage
     end
-   ARGV[0] 
+   ARGV[0]
 end
 
+$context_file = "./.ctx"
+
 def view
-    context_file = "./.ctx"
-    if File.exist? context_file
-        raw = File.read("./.ctx")
-        puts "Found ctx : #{raw}"
+    if File.exist? $context_file
+        raw = File.read $context_file
+        puts JSON.pretty_generate( JSON.parse(raw) )
     else
         puts "No local context"
     end
 end
 
+def set(key_path, value)
+    ctx = {}
+    if File.exist? $context_file
+        raw = File.read $context_file
+        puts "Updating existing context"
+        ctx = JSON.parse raw
+    end
 
+    key_index = 0
+    obj = ctx
+    while key_index < key_path.size - 1 do
+        if obj[key_path[key_index]].nil?
+            obj[key_path[key_index]] = {}
+        end
+        obj = obj[key_path[key_index]]
+        key_index += 1
+    end
+    obj[key_path[key_index]] = value
+
+    File.open($context_file, "w") {|f| f << JSON(ctx)}
+end
 
 case parse_command
 when "set"
-    
+   key_path = ARGV[1..-2]
+   value = ARGV[-1]
+   puts "Setting #{key_path} to value #{value}"
+   set(key_path, value)
 when "view"
     view
 when "use"
 
 else
-
+    usage
 end
