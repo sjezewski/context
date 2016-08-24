adeef3d67fdc1cd1ddb3b40cf81b409199d441bc
Sean Jezewski <seanwjezewski@gmail.com>
2016-08-23 23:38:49 -0700

    Update gcloud adapter
    Specifically, updating the gcloud config being used

--- adapter/gcloud.rb
+++ adapter/gcloud.rb
@@ -6,6 +6,30 @@ module Adapter
         end
         # Gets config from current environment's state
         def get_config
+        end
+        # Sets the current environment's state to the supplied value
+        def set_config(value)
+            valid_configs = get_valid_configs
+            if !valid_configs.include? value
+                puts "Cannot set gcloud config to #{value}"
+                valid_configs_string = valid_configs.collect {|c| "\t- #{c}"}.join("\n")
+                puts "Valid options are:\n#{valid_configs_string}"
+                puts "Update the local setting by running:"
+                puts "\t context set gcloud config <name>"
+                puts "To learn more about creating a gcloud config run:\n\tgcloud topic configurations"
+                exit 1
+            end
+            raw = `gcloud config configurations activate #{value}`
+            updated_config = get_config
+            if updated_config[@@field] != value
+                puts "Could not update config to #{value}.\n#{raw}"
+                exit 1
+            end
+        end
+
+        private
+
+        def list_valid_configs
             # Surprisingly, `gcloud config list` does not list the available configs
             # ... so we'll parse the results of the prompt from `gcloud init`
             p = IO.popen("gcloud init 2>/tmp/gc_config &", "w+")
@@ -13,33 +37,18 @@ module Adapter
             p.close_write
             raw = File.read("/tmp/gc_config")
             Process.kill("INT", p.pid)
-#  [3] Switch to and re-initialize existing configuration: [default]
-# [4] Switch to and re-initialize existing configuration: [personal]
+            configs = []
             raw.split("\n").each do |line|
                 # Find line that contains config name
+                # e.g:
+                # [4] Switch to and re-initialize existing configuration: [personal]
+                if line =~ /existing configuration.*?\[(.*?)\]/
+                    configs << $1
+                end
             end
+            configs
         end
-        # Sets the current environment's state to the supplied value
-        def set_config(value)
-            contexts = get_config()['contexts']
-            found = false
-            contexts.each do |context|
-                found = true if context["name"] == value
-            end
-            if !found
-                names = contexts.collect {|c| "\t- #{c['name']}"}.join("\n")
-                puts "Context '#{value}' not found in list of viable contexts:\n#{names}"
-                puts "Update the local setting by running:"
-                puts "\tcontext set kubectl context <name>"
-                exit 1
-            end
-            raw = `kubectl config use-context #{value}`
-            updated_config = get_config
-            if updated_config[@@field] != value
-                puts "Could not update config to #{value}.\n#{raw}"
-                exit 1
-            end
-        end
+
         end
     end
 end
