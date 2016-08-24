c5c1472a9dc1f448c3776451ba2bc4010ec46276
Sean Jezewski <seanwjezewski@gmail.com>
2016-08-23 23:57:18 -0700

    Finish output comment

--- context.rb
+++ context.rb
@@ -31,6 +31,7 @@ def view
         puts JSON.pretty_generate( JSON.parse(raw) )
     else
         puts "No local context"
+		puts "To get started refer to: https://github.com/sjezewski/context"
 		exit 1
     end
 end
