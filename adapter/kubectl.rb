59675f532683e03b4fd121d472c86deeb60b9d1b
Sean Jezewski <seanwjezewski@gmail.com>
2017-03-26 18:10:57 -0700

    Stip k8s adapter down more
    Don't need to set the state in there ... the context.sh script
    does the heavy lifting now

--- adapter/kubectl.rb
+++ adapter/kubectl.rb
@@ -7,7 +7,7 @@ module Adapter
         class << self
 
 		def supported?(setting)
-			["context", "init_cmd", "kubeconfig"].include? setting
+			["init_cmd"].include? setting
 		end
 
         # Uses the config loaded from a .ctx file to set the environment's state
@@ -16,16 +16,6 @@ module Adapter
 			unless cmd.nil?
 				system cmd
 			end
-
-            context_name = config['context']
-            return if context_name.nil?
-
-            set_config(context_name)
-        end
-        # Gets the current environment's state and saves it to a .ctx file
-        def update_config
-            #c = get_config
-            #set_config c[@@field]
         end
 
         end
